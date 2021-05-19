ffc3938084f882101acac01d178a32fe3a9f7cb5
Zhengyang Zhao <mason.zh@gmail.com>
2021-05-19 00:58:01 -0500

    A simple reactjs app with redux

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Smells like a tennage", duration: "3:05" },
+    { title: "Piano Man", duration: "5:25" },
+    { title: "Hello ", duration: "1:05" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
